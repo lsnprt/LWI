2649045b6416195dd57d06bd851cc32ff28a69e7
Alessandro Peretti <115103372+lsnprt@users.noreply.github.com>
2023-03-27 15:49:12 +0200

    Added DBContext + Migration

---
+++ LWI/Migrations/20230327134702_initial migration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LWI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<decimal>(type: "Money", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescriptionShort = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescriptionLong = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgAlt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
--- LWI/Program.cs
+++ LWI/Program.cs
@@ -1,13 +1,14 @@
 using LWI.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<DataService>();
-
 builder.Services.AddHttpContextAccessor();
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
 //builder.Services.ConfigureApplicationCookie(o => o.LoginPath="");
-
+builder.Services.AddDbContext<ApplicationContext>(o => o.UseSqlServer(connString));
 var app = builder.Build();
 
 
