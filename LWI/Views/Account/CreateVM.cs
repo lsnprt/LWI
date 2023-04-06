cdef3fe4eea1365e5941fb75c54c4494b32b2543
Alessandro Peretti <115103372+lsnprt@users.noreply.github.com>
2023-04-06 09:32:23 +0200

    Jämning av stil på formulärer

--- LWI/Views/Account/CreateVM.cs
+++ LWI/Views/Account/CreateVM.cs
@@ -4,38 +4,38 @@ namespace LWI.Views.Account
 {
     public class CreateVM
     {
-        [Required]
+        [Required(ErrorMessage = "Välj användarnamn")]
         [Display(Name = "Användarnamn")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Välj ett lösenord")]
         [Display(Name = "Lösenord")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Vänligen ange lösenordet igen")]
         [DataType(DataType.Password)]
         [Display(Name = "Upprepa lösenord")]
-        [Compare(nameof(Password))]
+        [Compare(nameof(Password), ErrorMessage = "Lösenorden matchar inte")]
         public string PasswordRepeat { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ange din mailadress")]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "E-mailadress")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ange ditt förnamn")]
         [Display(Name = "Förnamn")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ange ditt efternamn")]
         [Display(Name = "Efternamn")]
         public string LastName { get; set; }
 
         [Display(Name = "Yrke")]
-        public string Profession { get; set; }
+        public string? Profession { get; set; }
 
         [Display(Name = "Beskrivning")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
